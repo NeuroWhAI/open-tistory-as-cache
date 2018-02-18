0511b4405d7d98862981df4bab854abf7df6a8df
NeuroWhAI <tlsehdgus0212@gmail.com>
2018-02-18 01:56:11 +0000

    Add redirect flag

--- redirect-to-cache.js
+++ redirect-to-cache.js
@@ -1,3 +1,39 @@
+const KEY_REDIRECT = "neurowhai-OTAC-redirect";
+
+
+let flagRedirect = true;
+
+// Get first data
+chrome.storage.local.get(KEY_REDIRECT, function(items) {
+    if (items.length > 0) {
+        var data = items[0][KEY_REDIRECT];
+        
+        flagRedirect = ((data == "1") ? true : false);
+    }
+})
+
+// Apply change
+chrome.storage.onChanged.addListener(function(changes, areaName) {
+    if (areaName == 'local' && KEY_REDIRECT in changes) {
+        flagRedirect = ((changes[KEY_REDIRECT].newValue == "1") ? true : false);
+    }
+});
+
+// Change flag
+chrome.browserAction.onClicked.addListener(function(tab) {
+    chrome.storage.local.get(KEY_REDIRECT, function(items) {
+        if (items.length > 0) {
+            var data = items[0][KEY_REDIRECT];
+            
+            var newData = {};
+            newData[KEY_REDIRECT] = ((data == "1") ? "0" : "1");
+            
+            chrome.storage.local.set(newData);
+        }
+    })
+});
+
+
 function redirect(tabId, url) {
     chrome.tabs.update(tabId, {
         url: url,
@@ -19,6 +55,11 @@ function needRedirect(url) {
 }
 
 chrome.webNavigation.onBeforeNavigate.addListener(function(details) {
+    if (flagRedirect === false) {
+        return;
+    }
+    
+    
     // If only main window navigation
     if (details.frameId === 0 && needRedirect(details.url)) {
         // Redirect to Google web cache
@@ -29,6 +70,11 @@ chrome.webNavigation.onBeforeNavigate.addListener(function(details) {
 });
 
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+    if (flagRedirect === false) {
+        return;
+    }
+    
+    
     var url = changeInfo.url || tab.url;
     var title = changeInfo.title || tab.title;
 
